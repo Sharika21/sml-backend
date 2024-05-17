7f48c654b0897cc82df562b14815f0859a805748
Sharika Kanti Narsing <skantinarsing@gmail.com>
2024-05-17 20:12:40 +0200

    Create application

---
+++ sml-spring-boot/src/main/java/com/sml/smlspringboot/SmlSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sml.smlspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmlSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmlSpringBootApplication.class, args);
+	}
+
+}
---
+++ sml-spring-boot/src/test/java/com/sml/smlspringboot/SmlSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sml.smlspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmlSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
